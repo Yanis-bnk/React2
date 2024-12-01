9fb0d3aaf73d9c1ea89d9ff18ca83c7ebe5f4a47
yanis <benikenyanis1@gmail.com>
2024-12-01 14:16:23 +0100

    Premier commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Home extends Component {
+    render(){
+        return (
+     <div className="App">
+        <h1>Yanis beniken veut passer un message </h1>
+    
+    </div>
+
+        );
+
+    }
+}
+export default Home;
