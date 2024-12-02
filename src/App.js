c7a8a63840125b0d57d6f8c694f11331f7b08338
yanis <benikenyanis1@gmail.com>
2024-12-02 20:56:42 +0100

    Premier commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 
 
+import { Component } from 'react';
 import './App.css';
 import Home from './Home';
+class App extends Component{
+  state={
+    profile:[
+      {name:"yanis" , lastname:"beniken"},
+      {name:"rayan" , lastname:"hm"},
+      {name:"brahim" , lastname:"imanis"},
+      {name:"yan" , lastname:"ben"},
+      {name:"ra" , lastname:"h"},
+      {name:"br" , lastname:"imas"},
+      
+    ]
+  }
+  render (){
+    const data=this.state.profile.map((item)=>{
+      return (
+        <ul>
+          <li>{item.name}</li>
+          <li>{item.lastname}</li>
+        </ul>
+      )
 
-function App() {
-  return (
-    <div className="App">
-      
-      <Home name="yanis beniken" age ="25 "/>
-      
-        
-      
-    </div>
-  );
+    })
+    return (
+      <div className="App">
+        {data}
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
