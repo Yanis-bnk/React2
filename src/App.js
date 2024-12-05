1cae2c368fef4ccffbfda73a887fd9857810df5d
yanis <benikenyanis1@gmail.com>
2024-12-05 18:17:07 +0100

    Pre

--- src/App.js
+++ src/App.js
@@ -2,48 +2,29 @@
 
 import { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route ,Link} from 'react-router-dom';
+
+import Home from './component/Home';
+
+
 
 class App extends Component{
-  constructor(props){
-    super(props)
-    this.state={
-      dz:undefined,
-      maroco:undefined
-    }
-  }
-  handleChange1=(e)=> {
-    if(e.target.checked==true && e.target.name=="dz"){
-      this.setState({
-        
-        dz:'dz'
-      })
-    }
-
-    
-    }
-  handleChange=(e)=> {
-    if(e.target.checked==true && e.target.name=="maroco"){
-      this.setState({
-        
-        maroco:'maroco'
-      })
-    }
-
-    
-    }
-
+ 
   render (){
     
     return (
-      <div className="App">
-           
-             
-             <br/>
-             dz: <input type='checkbox' name='dz' value='dz' onChange={this.handleChange1}/>
-             maroco: <input type='checkbox' name='maroco' value='maroco' onChange={this.handleChange}/>
-             <p>{this.state.dz}</p>
-             <p>{this.state.maroco}</p>
-      </div>
+      <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/About">About</Link>
+      <Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+ </Router>
+
+      
+      
+      </BrowserRouter>
     )
   }
 }
---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Home extends Component{
+    render(){
+        return(<div>
+
+
+            <h1>je suis dans le Home</h1>
+        </div>)
+    }
+}
